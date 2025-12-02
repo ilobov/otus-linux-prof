c6616ed3fa87d0b594d590d0e64feb09e96fcf6b
ilobov <ilobov@yandex.ru>
2025-12-02 14:39:45 +0300

    project fix replica settings

--- project/ansible/db_backup_role/templates/run_backup.sh
+++ project/ansible/db_backup_role/templates/run_backup.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/usr/bin/mysqldump --single-transaction --all-databases --routines --triggers --events > /backup/mysql/all-$(date +'%F_%T').sql
+/usr/bin/mysqldump --single-transaction --all-databases --routines --triggers --events --set-gtid-purged=OFF > /backup/mysql/all-$(date +'%F_%T').sql
